41240926d07e4f9afd116a4270ad102aa837e60e
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2022-03-17 18:15:09 +0530

    add: added homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { Header } from "./components/header.component/Header";
+import { Header } from "./components/Header.component/Header";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
 
 const App = () => (
-  <>
+  <div className="e-commerce-app-container">
     <Header />
     {/* <Login /> */}
-    <SignUp />
-  </>
+    {/* <SignUp /> */}
+    <Home />
+  </div>
 );
 
 export default App;
